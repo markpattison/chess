e5cc9cc4cdf7126fdf70f87859b3089970461676
markpattison <mr.pattison@gmail.com>
2021-03-13 08:45:52 +0000

    extend perft tests

--- tests/Domain.UnitTests/MoveGenerationTests.fs
+++ tests/Domain.UnitTests/MoveGenerationTests.fs
@@ -487,19 +487,19 @@ let queenTests =
 
 // see https://www.chessprogramming.org/Perft_Results
 
-let testPositions =
+let perftTests =
     [
-        "Initial position", getInitialPosition(), [ 20 ]
-        "Position 2", fromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), [ 48 ]
-        "Position 3", fromFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"), [ 14 ]
-        "Position 4", fromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"), [ 6 ]
-        "Position 4 mirrored", fromFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"), [ 6 ]
-        "Position 5", fromFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"), [ 44 ]
-        "Position 6", fromFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"), [ 46 ]
+        "Initial position", getInitialPosition(), [ 20; 400; 8902 ]
+        "Position 2", fromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), [ 48; 2039; 97862 ]
+        "Position 3", fromFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"), [ 14; 191; 2812 ]
+        "Position 4", fromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"), [ 6; 264; 9467 ]
+        "Position 4 mirrored", fromFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"), [ 6; 264; 9467 ]
+        "Position 5", fromFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"), [ 44; 1486; 62379 ]
+        "Position 6", fromFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"), [ 46; 2079; 89890 ]
     ]
 
 let testPerft name position depth expectedPerft =
-    test name {
+    test (sprintf "Depth %i" depth) {
         let result = perft depth position
 
         Expect.equal result expectedPerft (sprintf "Perft depth %i for %s should be %i" depth name expectedPerft)
@@ -508,10 +508,10 @@ let testPerft name position depth expectedPerft =
 let testAllPerft (name, position, expectedResults) =
     expectedResults
     |> List.mapi (fun depthMinusOne expectedPerft -> testPerft name position (depthMinusOne + 1) expectedPerft)
-    |> testList (sprintf "Perft results for %s" name)
+    |> testList name
 
 let allPerftTests =
-    testPositions |> List.map testAllPerft |> testList "Correct perft results"
+    perftTests |> List.map testAllPerft |> testList "Perft"
 
 let tests =
     testList "Potential move generation tests" [
