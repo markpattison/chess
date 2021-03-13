2db5be3fa2f5225715653f78999621d443a3dcc2
markpattison <mr.pattison@gmail.com>
2021-03-13 09:42:32 +0000

    added longer perft test (not run automatically)

--- tests/Domain.UnitTests/MoveGenerationTests.fs
+++ tests/Domain.UnitTests/MoveGenerationTests.fs
@@ -487,15 +487,34 @@ let queenTests =
 
 // see https://www.chessprogramming.org/Perft_Results
 
-let perftTests =
+let perfTestPosition1 = "Initial position", getInitialPosition()
+let perfTestPosition2 = "Position 2", fromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
+let perfTestPosition3 = "Position 3", fromFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1")
+let perfTestPosition4 = "Position 4", fromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")
+let perfTestPosition4Mirrored = "Position 4 mirrored", fromFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1")
+let perfTestPosition5 = "Position 5", fromFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
+let perfTestPosition6 = "Position 6", fromFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10")
+
+let perftTestData =
     [
-        "Initial position", getInitialPosition(), [ 20; 400; 8902 ]
-        "Position 2", fromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), [ 48; 2039; 97862 ]
-        "Position 3", fromFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"), [ 14; 191; 2812 ]
-        "Position 4", fromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"), [ 6; 264; 9467 ]
-        "Position 4 mirrored", fromFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"), [ 6; 264; 9467 ]
-        "Position 5", fromFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"), [ 44; 1486; 62379 ]
-        "Position 6", fromFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"), [ 46; 2079; 89890 ]
+        perfTestPosition1, [ 20; 400; 8902; 197281 ]
+        perfTestPosition2, [ 48; 2039; 97862 ]
+        perfTestPosition3, [ 14; 191; 2812; 43238 ]
+        perfTestPosition4, [ 6; 264; 9467 ]
+        perfTestPosition4Mirrored, [ 6; 264; 9467 ]
+        perfTestPosition5, [ 44; 1486; 62379 ]
+        perfTestPosition6, [ 46; 2079; 89890 ]
+    ]
+
+let slowPerfTestData =
+    [
+        perfTestPosition1, [ 5, 4865609 ]
+        perfTestPosition2, [ 4, 4085603 ]
+        perfTestPosition3, [ 5, 674624 ]
+        perfTestPosition4, [ 4, 422333 ]
+        perfTestPosition4Mirrored, [ 4, 422333 ]
+        perfTestPosition5, [ 4, 2103487 ]
+        perfTestPosition6, [ 4, 3894594 ]        
     ]
 
 let testPerft name position depth expectedPerft =
@@ -505,13 +524,21 @@ let testPerft name position depth expectedPerft =
         Expect.equal result expectedPerft (sprintf "Perft depth %i for %s should be %i" depth name expectedPerft)
     }
 
-let testAllPerft (name, position, expectedResults) =
+let testPerftList ((name, position), expectedResults) =
     expectedResults
     |> List.mapi (fun depthMinusOne expectedPerft -> testPerft name position (depthMinusOne + 1) expectedPerft)
     |> testList name
 
-let allPerftTests =
-    perftTests |> List.map testAllPerft |> testList "Perft"
+let mainPerftTests =
+    perftTestData |> List.map testPerftList |> testList "Perft"
+
+let testSlowPerfList ((name, position), expectedResults) =
+    expectedResults
+    |> List.map (fun (depth, expectedPerft) -> testPerft name position depth expectedPerft)
+    |> testList name
+
+let slowPerftTests =
+    slowPerfTestData |> List.map testSlowPerfList |> ptestList "Slow perft"
 
 let debugTest =
     ptest "Debug" {
@@ -532,6 +559,7 @@ let tests =
         bishopTests
         rookTests
         queenTests
-        allPerftTests
+        mainPerftTests
+        slowPerftTests
         debugTest
     ]
